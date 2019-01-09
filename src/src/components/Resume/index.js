36d47017671add6f00b6735c5520f7edb3ecfe97
Mark Ernest R. Matute <markernest.matute@gmail.com>
2019-01-09 15:42:37 +0800

    Update projects.

--- src/src/components/Resume/index.js
+++ src/src/components/Resume/index.js
@@ -25,7 +25,7 @@ export default class Resume extends React.Component {
 
         <div className="page-footer">
           <div className="resume-buttons">
-            <a href="MarkErnest.pdf" target="_blank" className="btn btn-default btn-default2"><i className="fa fa-download"></i>&nbsp; Download my resume</a>
+            <a href="https://github.com/MarkMatute/MarkMatute.github.io/raw/master/MarkErnest.pdf" target="_blank" className="btn btn-default btn-default2"><i className="fa fa-download"></i>&nbsp; Download my resume</a>
             <a href="#page-contact" className="btn btn-default btn-default2 link-page"><i className="fa fa-download"></i>&nbsp;  Get in Touch</a>
           </div>
         </div>
