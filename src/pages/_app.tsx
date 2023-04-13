31691d0d0e229eb79ee3d1f65b1dcb8efbb4c2e6
Mark Ernest R. Matute <markernest.matute@gmail.com>
2023-04-14 07:07:06 +0800

    - Enabled google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 
 import $ from 'jquery';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   /**
@@ -19,6 +20,20 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      {/* Google Analytics */}
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-LR4EB63VR5`} />
+
+      <Script strategy="lazyOnload" id="ga-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LR4EB63VR5', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
